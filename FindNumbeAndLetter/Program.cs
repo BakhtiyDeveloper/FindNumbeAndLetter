8494c6a9d986d3bad88ec4d317d50296ec659e47
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-07-03 23:36:52 +0500

    Initialise project

--- FindNumbeAndLetter/Program.cs
+++ FindNumbeAndLetter/Program.cs
@@ -6,7 +6,33 @@ namespace FindNumberAndLetter
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This program determines the number of letters and numbers in the entered text");
+
+            Console.Write("Enter the text: ");
+            string userInput = Console.ReadLine();
+
+            int lettersCount = 0;
+            int numbersCount = 0;
+
+            string letters = "";
+            string numbers = "";
+
+            foreach (char c in userInput)
+            {
+                if (char.IsDigit(c))
+                {
+                    numbers += c;
+                    numbersCount++;
+                }
+                else if (char.IsLetter(c))
+                {
+                    letters += c;
+                    lettersCount++;
+                }
+            }
+
+            Console.WriteLine($"the number of letters in the text : {lettersCount}");
+            Console.WriteLine($"the number of numbers in the text : {numbersCount}");
         }
     }
 }
